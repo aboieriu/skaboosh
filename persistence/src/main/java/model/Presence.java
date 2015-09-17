a808bb3888ffc4805810198ae9fd6bf2e14ed1d5
Ionutz Cringanu <cringanu_ionutz@yahoo.ro>
2015-09-17 12:30:29 +0300

    comment

--- persistence/src/main/java/model/Presence.java
+++ persistence/src/main/java/model/Presence.java
@@ -1,11 +1,64 @@
 package model;
 
-/**
- * Created with IntelliJ IDEA.
- * User: User
- * Date: 17.09.2015
- * Time: 11:48
- * To change this template use File | Settings | File Templates.
- */
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
 public class Presence {
+
+
+    @Id
+    @GeneratedValue(generator = "idIncrementor")
+    @GenericGenerator(name = "idIncrementor", strategy = "increment")
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "groupId")
+    private String groupId;
+    @Column(name = "userId")
+    private String userId;
+    @Column(name = "date")
+    private Date date;
+
+    public Presence(){}
+
+    public Presence(String groupId, String userId, Date date) {
+        this.groupId = groupId;
+        this.userId = userId;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
