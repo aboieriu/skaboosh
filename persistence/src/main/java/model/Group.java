6e2b5a217595a36fd33d394e067ae3c823afda3b
diana05 <puffy_dya17@yahoo.com>
2015-09-17 12:30:08 +0300

    coment

--- persistence/src/main/java/model/Group.java
+++ persistence/src/main/java/model/Group.java
@@ -1,5 +1,10 @@
 package model;
 
+ import javax.persistence.*;
+ import org.hibernate.annotations.GenericGenerator;
+
+ import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: User
@@ -7,5 +12,59 @@ package model;
  * Time: 11:48
  * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Table(name="group")
 public class Group {
+    @Id()
+    @GeneratedValue(generator="idIncrementor")
+    @GenericGenerator(name="idIncrementor" , strategy="increment")
+
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column (name="startDate")
+    private Date startDate ;
+    @Column(name="endDate")
+    private Date endDate;
+
+    public Group(){};
+
+    public Group(String name, Date startDate, Date endDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
 }
+
