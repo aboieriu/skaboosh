1e2a270d618e4a85994f8f5e723a0b8f34217373
diana05 <puffy_dya17@yahoo.com>
2015-09-17 11:45:17 +0300

    base

---
+++ impl/src/main/java/facade/UserFacade.java
@@ -0,0 +1,11 @@
+package facade;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 17.09.2015
+ * Time: 11:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserFacade {
+}
