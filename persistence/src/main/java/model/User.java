5215b737ae04939eb345e5a5f23d6f3d170c89e8
CatalinVlad <cata_bv94@yahoo.com>
2015-09-17 12:30:44 +0300

    coment

--- persistence/src/main/java/model/User.java
+++ persistence/src/main/java/model/User.java
@@ -1,5 +1,9 @@
 package model;
 
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
 /**
  * Created with IntelliJ IDEA.
  * User: User
@@ -7,5 +11,113 @@ package model;
  * Time: 11:48
  * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Table(name="user")
 public class User {
+    @GeneratedValue(generator = "idIncrementor")
+    @GenericGenerator(name = "idIncrementor", strategy = "increment")
+    @Id
+    private Long Id;
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+    @Column(name="username")
+    private String username;
+    @Column(name="password")
+    private String password;
+    @Column(name="email")
+    private String email;
+    @Column(name="phone")
+    private String phone;
+    @Column(name="repositoryUrl")
+    private String repositoryUrl;
+    @Column(name="groupId")
+    private Long groupId;
+
+    public User(String name, String surname, String username, String password, String email, String phone, String repositoryUrl, Long groupId) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.repositoryUrl = repositoryUrl;
+        this.groupId = groupId;
+    }
+
+    private User(){
+
+    }
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
+
+    public void setRepositoryUrl(String repositoryUrl) {
+        this.repositoryUrl = repositoryUrl;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
 }
