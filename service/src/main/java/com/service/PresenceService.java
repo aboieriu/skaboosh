31ebd6e356a68973935acc48859da03651f3f92a
diana05 <puffy_dya17@yahoo.com>
2015-09-17 11:52:45 +0300

    comment

---
+++ service/src/main/java/com/service/PresenceService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 17.09.2015
+ * Time: 11:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class PresenceService {
+}
