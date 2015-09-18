9bd32eff874075874dd4c83a6177fa3aff37f8bb
CatalinVlad <cata_bv94@yahoo.com>
2015-09-18 10:41:01 +0300

    Presence

--- service/src/main/java/com/service/CorsFilter.java
+++ service/src/main/java/com/service/CorsFilter.java
@@ -1,7 +1,40 @@
 package com.service;
 
+
+import java.io.*;
+
+
+
+
 /**
- * Created by Johnny on 9/17/2015.
+ * Created by CataVlad on 15-Sep-15.
  */
-public class CorsFilter {
+public class CorsFilter implements Filter {
+
+    public void destroy() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        httpServletResponse.addHeader("Access-Control-Allow-Origin","*");
+        httpServletResponse.addHeader("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE, PUT");
+        httpServletResponse.addHeader("Access-Control-Max-Age","3600");
+        httpServletResponse.addHeader("Access-Control-Allow-Headers","x-requested-with, X-Auth-Token, Content-Type");
+
+        filterChain.doFilter(request, response);
+    }
+
+
+
+    public void init(FilterConfig arg0) throws ServletException {
+        // TODO Auto-generated method stub
+
+    }
 }
