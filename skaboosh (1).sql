624831171e180690e610f8d757ceece791f68af0
Ionutz Cringanu <cringanu_ionutz@yahoo.ro>
2015-09-28 10:33:07 +0300

    Presence

---
+++ skaboosh (1).sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2015 at 09:32 AM
+-- Server version: 5.6.26
+-- PHP Version: 5.5.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skaboosh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `presence`
+--
+
+CREATE TABLE IF NOT EXISTS `presence` (
+  `id` int(11) NOT NULL,
+  `groupId` varchar(45) DEFAULT NULL,
+  `userId` varchar(45) DEFAULT NULL,
+  `date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `presence`
+--
+
+INSERT INTO `presence` (`id`, `groupId`, `userId`, `date`) VALUES
+(1, '1', '1', '2015-09-14 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_group`
+--
+
+CREATE TABLE IF NOT EXISTS `student_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `startDate` datetime DEFAULT NULL,
+  `endDate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_group`
+--
+
+INSERT INTO `student_group` (`id`, `name`, `startDate`, `endDate`) VALUES
+(1, 'Cata', '2010-10-20 12:00:00', '2020-12-20 14:00:00'),
+(2, 'Ionut', '2015-10-02 00:00:00', '2015-11-02 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `surname` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `repositoryUrl` varchar(45) DEFAULT NULL,
+  `groupId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `surname`, `username`, `password`, `email`, `phone`, `repositoryUrl`, `groupId`) VALUES
+(1, 'cata', 'vlad', 'batman', 'pute', 'salam@', '95315151351', 'www.yahoo.com', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `presence`
+--
+ALTER TABLE `presence`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Indexes for table `student_group`
+--
+ALTER TABLE `student_group`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
